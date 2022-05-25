cebc84d7126fbeadf67cc6c0dba052048e7b0980
Jorge Mollericon <jlmollericon@gmail.com>
2022-05-25 11:41:40 -0400

    🎨 implement a base buttons to increment and decrement the counter

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const counter = useSelector(state => state.counter);
+  const increment = () => {
+
+  };
+  const decrement = () => {
+
+  };
 
   return (
     <div className="App">
       <header className="App-content">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Counter: <code>{counter}</code></p>
+        <div className="buttons-container">
+          <button className="App-button" onClick={increment}>+</button>
+          <button className="App-button" onClick={decrement}>-</button>
+        </div>
       </header>
     </div>
   );
